7569134442755e1fd0d0787daf2119df7968c706
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-07-17 21:21:45 +0500

    feat: Added codes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,38 +13,48 @@ export const About = (props) => {
             <div className="about-text">
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              {/* <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <h3>Farooq A. Kirmani-Massala</h3>
-                  <ul>
-                    <li>MBA from Neoma Business School, Reims, France</li>
+              <div className="list-style">
+                <div
+                  className="col-lg-6 col-sm-6 col-xs-12"
+                  style={{ padding: "0px", width: "100%" }}
+                >
+                  <h3>
+                    Reasons why a customer should choose British Analytica
+                  </h3>
+                  <ul style={{ padding: "0px" }}>
                     <li>
-                      v M.Sc. Strategic Project Management from Hariot-Watt
-                      University, Edinburgh, UK; Politecnico di Milano, Italy;
-                      Umeà University, Sweden.
+                      Global Expertise, Local Knowledge: British Analytica
+                      leverages the best of both worlds by combining British
+                      consulting experience with deep understanding of local
+                      markets. This ensures their solutions are effective and
+                      tailored to your specific needs.
                     </li>
                     <li>
-                      +20 years' experience of working with 14 different
-                      organisations across the UK and Asia
+                      Holistic Business Assessment: They don't offer
+                      one-size-fits-all solutions. British Analytica takes the
+                      time to comprehensively assess your entire business before
+                      crafting a bespoke strategy.
+                    </li>
+                    <li>
+                      Bespoke Solutions: British Analytica believes in
+                      customized solutions. They will develop a plan that دقیقا
+                      (daqīqa) addresses your unique challenges and
+                      opportunities.
+                    </li>
+                    <li>
+                      Proven Track Record: With a proven track record of
+                      success, British Analytica can give you the confidence
+                      that their solutions will deliver the results you need.
+                    </li>
+                    <li>
+                      Long-Term Partnership: British Analytica is committed to
+                      building long-term partnerships with their clients. They
+                      will continue to support you as you implement their
+                      recommendations and achieve your goals.
                     </li>
                   </ul>
                 </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <h3>Diyorbek Muradov</h3>
-                  <ul>
-                    <li>Bachelor of International Economic Relations, TIU</li>
-                    <li>
-                      PMP-30, Rank 1, Japan International Cooperation Agency
-                    </li>
-                    <li>
-                      Keiei-Juku, Strategic Development of Business Executives
-                    </li>
-                    <li>
-                      +15 years' experience of consulting services in Uzbekistan
-                    </li>
-                  </ul>
-                </div>
-              </div> */}
+              </div>
             </div>
           </div>
         </div>

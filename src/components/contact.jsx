e4911a46e3d5dfc47e2cc60abaf9043d8e2b593b
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-07-17 23:16:08 +0500

    feat: Added codes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 import React from "react";
+import "../assets/index.css";
 
 const initialState = {
   name: "",
@@ -45,172 +46,188 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+          <div
+            className="contact__container"
+            style={{
+              width: "100%",
+              padding: "50px",
+              minHeight: "100%",
+              borderRadius: "10px",
+              backgroundColor: "rgba(255, 255, 255, 0.2)",
+              border: "2px solid rgba(255, 255, 255, 0.4)",
+              backdropFilter: "blur(5px)",
+            }}
+          >
+            <div className="col-md-8">
+              <div className="row">
+                <div className="section-title">
+                  <h2>Get In Touch</h2>
+                  <p>
+                    Please fill out the form below to send us an email and we
+                    will get back to you as soon as possible.
+                  </p>
+                </div>
+                <form name="sentMessage" validate onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="form-control"
+                          placeholder="Name"
+                          required
+                          onChange={handleChange}
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          required
+                          onChange={handleChange}
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="message"
+                      className="form-control"
+                      rows="4"
+                      placeholder="Message"
+                      required
+                      onChange={handleChange}
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div id="success"></div>
+                  <button type="submit" className="btn btn-custom btn-lg">
+                    Send Message
+                  </button>
+                </form>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
             </div>
-          </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              {/* <p>
+            <div className="col-md-3 col-md-offset-1 contact-info">
+              <div className="contact-item">
+                <h3>Contact Info</h3>
+                {/* <p>
                 <span>
                   <i className="fa fa-map-marker"></i> Address
                 </span>
                 {props.data.address}
               </p> */}
-            </div>
-            <div className="contact-item">
-              <a href="tel:+998998993515">
-                <p
-                  style={{
-                    gap: "20px",
-                    display: "flex",
-                    color: "white",
-                    alignItems: "center",
-                  }}
-                >
-                  <span
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "0px",
-                    }}
-                  >
-                    <i className="fa fa-phone"></i> Phone
-                  </span>{" "}
-                  +998998993515
-                </p>
-              </a>
-            </div>
-            <div className="contact-item">
-              <a href="mailto:diyorbek.britishanalytica@gmail.com">
-                <p
-                  style={{
-                    gap: "20px",
-                    display: "flex",
-                    color: "white",
-                    alignItems: "center",
-                  }}
-                >
-                  <span
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "0px",
-                    }}
-                  >
-                    <i className="fa fa-envelope-o"></i> Email
-                  </span>{" "}
-                  diyorbek.britishanalytica@gmail.com
-                </p>
-              </a>
-            </div>
-            <div className="contact-item">
-              <a href="https://t.me/MurodovDiyorbek" target="_blank">
-                <p
-                  style={{
-                    gap: "20px",
-                    display: "flex",
-                    color: "white",
-                    alignItems: "center",
-                  }}
-                >
-                  <span
-                    style={{
-                      gap: "10px",
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "0px",
-                    }}
-                  >
-                    <i className="fab fa-telegram"></i> Telegram
-                  </span>
-                  MurodovDiyorbek
-                </p>
-              </a>
-            </div>
-            <div className="contact-item">
-              <a
-                style={{ display: "flex", color: "white", width: "100%" }}
-                href="https://t.me/MurodovDiyorbek"
-                target="_blank"
+              </div>
+              <ul
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(2,auto)",
+                }}
               >
-                <p
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    color: "white",
-                    alignItems: "center",
-                  }}
-                >
-                  <span
-                    style={{
-                      gap: "10px",
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "0px",
-                    }}
+                <div className="contact-item">
+                  <a href="tel:+998998993515">
+                    <p
+                      style={{
+                        gap: "20px",
+                        display: "flex",
+                        color: "white",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          marginBottom: "0px",
+                        }}
+                      >
+                        <i className="fa fa-phone"></i> Phone
+                      </span>{" "}
+                    </p>
+                  </a>
+                </div>
+                <div className="contact-item">
+                  <a href="mailto:diyorbek.britishanalytica@gmail.com">
+                    <p
+                      style={{
+                        gap: "20px",
+                        display: "flex",
+                        color: "white",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          marginBottom: "0px",
+                        }}
+                      >
+                        <i className="fa fa-envelope-o"></i> Email
+                      </span>{" "}
+                    </p>
+                  </a>
+                </div>
+                <div className="contact-item">
+                  <a href="https://t.me/MurodovDiyorbek" target="_blank">
+                    <p
+                      style={{
+                        gap: "20px",
+                        display: "flex",
+                        color: "white",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span
+                        style={{
+                          gap: "10px",
+                          display: "flex",
+                          alignItems: "center",
+                          marginBottom: "0px",
+                        }}
+                      >
+                        <i className="fab fa-telegram"></i> Telegram
+                      </span>
+                    </p>
+                  </a>
+                </div>
+                <div className="contact-item">
+                  <a
+                    style={{ display: "flex", color: "white", width: "100%" }}
+                    href="https://t.me/MurodovDiyorbek"
+                    target="_blank"
                   >
-                    <i className="fa-solid fa-location-dot"></i> Adress
-                  </span>
-                </p>
-                Tirsakobod 4/194a, Tashkent, Uzbekistan
-              </a>
+                    <p
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        color: "white",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span
+                        style={{
+                          gap: "10px",
+                          display: "flex",
+                          alignItems: "center",
+                          marginBottom: "0px",
+                        }}
+                      >
+                        <i className="fa-solid fa-location-dot"></i> Adress
+                      </span>
+                    </p>
+                  </a>
+                </div>
+              </ul>
             </div>
           </div>
         </div>

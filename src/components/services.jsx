a9079c378685fae630375d50ae545c5a38479fdb
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-07-17 23:02:24 +0500

    feat: Added services background color

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 export const Services = (props) => {
   return (
-    <div id="values" className="text-center">
+    <div
+      id="values"
+      className="text-center"
+      style={{
+        color: "white",
+        padding: "20px",
+        boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.05)",
+        background: "linear-gradient(to right, #6372ff 0%, #5ca9fb 100%)",
+      }}
+    >
       <div className="container">
         <div className="section-title">
-          <h2>Our values</h2>
+          <h2 style={{ color: "white" }}>Our values</h2>
         </div>
         <div className="grid">
           {props.data
@@ -15,7 +24,7 @@ export const Services = (props) => {
                     <i style={{ fontSize: "25px" }} className={d.icon}></i>
                   </div>
                   <div className="service-desc">
-                    <h3>{d.name}</h3>
+                    <h3 style={{ color: "white" }}>{d.name}</h3>
                     <p>{d.text}</p>
                   </div>
                 </div>
